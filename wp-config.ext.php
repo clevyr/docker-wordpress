090c46ea9164d2e3c69cf430178924f1396a4fee
Gabe Cook <git@gabe565.com>
2026-06-09 11:39:43 -0500

    fix: change config overrides to skip null values

--- wp-config.ext.php
+++ wp-config.ext.php
@@ -1,15 +1,24 @@
-define('DISABLE_WP_CRON', getenv_docker('DISABLE_WP_CRON', true));
+if (!function_exists('define_docker_env')) {
+    function define_docker_env($name, $default = null) {
+        $value = getenv_docker($name, $default);
+        if ($value !== null) {
+            define($name, $value);
+        }
+    }
+}
+
+define_docker_env('DISABLE_WP_CRON', true);
 
 /* C3 Cloudfront Clear Cache envs */
-define('AWS_ACCESS_KEY_ID', getenv_docker('AWS_ACCESS_KEY_ID', null));
-define('AWS_SECRET_ACCESS_KEY', getenv_docker('AWS_SECRET_ACCESS_KEY', null));
-define('C3_DISTRIBUTION_ID', getenv_docker('C3_DISTRIBUTION_ID', null));
-define('C3_DISTRIBUTION_TENANT_ID', getenv_docker('C3_DISTRIBUTION_TENANT_ID', null));
+define_docker_env('AWS_ACCESS_KEY_ID');
+define_docker_env('AWS_SECRET_ACCESS_KEY');
+define_docker_env('C3_DISTRIBUTION_ID');
+define_docker_env('C3_DISTRIBUTION_TENANT_ID');
 
 /* rhubarbgroup/redis-cache envs */
-define('WP_REDIS_HOST', getenv_docker('WP_REDIS_HOST', null));
-define('WP_REDIS_PORT', getenv_docker('WP_REDIS_PORT', 6379));
-define('WP_REDIS_PASSWORD', getenv_docker('WP_REDIS_PASSWORD', null));
-define('WP_REDIS_DATABASE', getenv_docker('WP_REDIS_DATABASE', 0));
-define('WP_REDIS_PREFIX', getenv_docker('WP_REDIS_PREFIX', null));
-define('WP_REDIS_MAXTTL', getenv_docker('WP_REDIS_MAXTTL', 0));
+define_docker_env('WP_REDIS_HOST');
+define_docker_env('WP_REDIS_PORT');
+define_docker_env('WP_REDIS_PASSWORD');
+define_docker_env('WP_REDIS_DATABASE');
+define_docker_env('WP_REDIS_PREFIX');
+define_docker_env('WP_REDIS_MAXTTL');
